47ec45ea1053865962f5bd54ed3b888b1aee8b0c
ligetitamas <ltommipresents@gmail.com>
2025-06-21 11:16:20 +0200

    Created /mobhunt command and basic usage

--- src/main/java/org/ltommi/mobHunt/Main.java
+++ src/main/java/org/ltommi/mobHunt/Main.java
@@ -1,6 +1,7 @@
 package org.ltommi.mobHunt;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.ltommi.mobHunt.commands.MobHuntCommand;
 
 public final class Main extends JavaPlugin {
 
@@ -8,12 +9,18 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         saveDefaultConfig();
+
         mobHuntManager = new MobHuntManager(this);
+
+        this.getCommand("mobhunt").setExecutor(new MobHuntCommand(this));
+
         getLogger().info("MobHunt has been successfully loaded!");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+    }
+    public MobHuntManager GetMobHuntManager(){
+        return mobHuntManager;
     }
 }
